a86c4268aaa83d1dcf0d7ce75334b5cb155c9a92
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2024-12-25 10:21:13 +0700

    M UI

--- script.js
+++ script.js
@@ -1,74 +1,73 @@
-class Slider{
-    constructor(_element){
-      this.slider = _element;
-      this.items = [this.slider.querySelector('.item-1'), this.slider.querySelector('.item-2'), this.slider.querySelector('.item-3')];
-      this.background = this.slider.querySelector('.background');
+class Slider {
+  constructor(_element) {
+    this.slider = _element;
+    this.items = Array.from(this.slider.querySelectorAll('.item'));
+    this.background = this.slider.querySelector('.background');
+    this.listeners();
+    this.setBackground();
+    this.updateDescriptionVisibility();
+  }
+
+  updateDescriptionVisibility = () => {
+    this.items.forEach((item, index) => {
+      const description = item.querySelector('.item-description');
+      if (index === 1) {
+        description.style.opacity = '1';
+        description.style.transform = 'scale(1)';
+      } else {
+        description.style.opacity = '0';
+        description.style.transform = 'scale(0.8)';
+      }
+    });
+  }
+
+  setBackground = () => {
+    console.log(this.background);
+    let pic = this.items[0].querySelector('img').getAttribute('src');
+    this.background.style.background = `linear-gradient(180deg, rgba(0,0,0,0.6),rgba(0,0,0,0.3)), url(${pic}) no-repeat center fixed`
+    this.background.style.backgroundSize = `cover`;
+  }
+
+  item_3Click = () => {
+      this.items[0].removeEventListener('mousedown', this.item_1Click);
+      this.items[2].removeEventListener('mousedown', this.item_3Click);
+      this.next();
+  }
+
+  item_1Click = () => {
+      this.items[0].removeEventListener('mousedown', this.item_1Click);
+      this.items[2].removeEventListener('mousedown', this.item_3Click);
+      this.previous();
+  }
+
+  listeners = () => {
+      this.items[2].addEventListener('mousedown', this.item_3Click);
+      this.items[0].addEventListener('mousedown', this.item_1Click);
+  }
+
+  reclass = () => {
+    for(let i = 0; i < this.items.length; i++){
+      this.items[i].classList.remove('item-1', 'item-2', 'item-3');
+      this.items[i].classList.add('item-' + String(i + 1))
+    }
+    
+  }
+  
+  next = () => {
+    this.items.unshift(this.items[2]);
+    this.items.pop();
+    this.reclass();
+    this.listeners();
+    this.setBackground();
+  }
+
+  previous = () => {
+      this.items.push(this.items[0]);
+      this.items.shift();
+      this.reclass();
       this.listeners();
       this.setBackground();
-    }
+  }
+}
 
-    setBackground = () => {
-      console.log(this.background);
-      let pic = this.items[0].querySelector('img').getAttribute('src');
-      this.background.style.background = `linear-gradient(180deg, rgba(0,0,0,0.6),rgba(0,0,0,0.3)), url(${pic}) no-repeat center fixed`
-      this.background.style.backgroundSize = `cover`;
-    }
-  
-    item_3Click = () => {
-        this.items[0].removeEventListener('mousedown', this.item_1Click);
-        this.items[2].removeEventListener('mousedown', this.item_3Click);
-        this.next();
-    }
-
-    item_1Click = () => {
-        this.items[0].removeEventListener('mousedown', this.item_1Click);
-        this.items[2].removeEventListener('mousedown', this.item_3Click);
-        this.previous();
-    }
-
-    listeners = () => {
-        this.items[2].addEventListener('mousedown', this.item_3Click);
-        this.items[0].addEventListener('mousedown', this.item_1Click);
-    }
-
-    reclass = () => {
-      for(let i = 0; i < this.items.length; i++){
-        this.items[i].classList.remove('item-1', 'item-2', 'item-3');
-        this.items[i].classList.add('item-' + String(i + 1))
-      }
-      
-    }
-    
-    next = () => {
-        this.items.unshift(this.items[2]);
-        this.items.pop();
-        this.reclass();
-        this.updateDescriptionVisibility();
-        this.listeners();
-        this.setBackground();
-      }
-  
-      previous = () => {
-          this.items.push(this.items[0]);
-          this.items.shift();
-          this.reclass();
-          this.updateDescriptionVisibility();
-          this.listeners();
-          this.setBackground();
-      }
-  
-      updateDescriptionVisibility = () => {
-        this.items.forEach((item, index) => {
-          const description = item.querySelector('.item-description');
-          if (index === 1) {
-            description.style.transform = 'translateY(0)';
-            description.style.opacity = '1';
-          } else {
-            description.style.transform = 'translateY(100%)';
-            description.style.opacity = '0';
-          }
-        });
-      }
-    }
-    
-    const slider = new Slider(document.querySelector('#slider'));
+const slider = new Slider(document.querySelector('#slider'));
